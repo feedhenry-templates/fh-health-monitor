04ba73c0e455f9cee1f89bff6ac0f0e29f143201
Keyang <keyang.xiang@gmail.com>
2014-09-29 15:28:53 +0100

    change def db

--- env.js
+++ env.js
@@ -5,7 +5,7 @@ var sysEnv=process.env;
 var hardCoded={
   "staticFolder":__dirname+"/client/",
   "TIMER_INTERVAL":10,
-  "FH_MONGODB_CONN_URL":"mongodb://fhmonitor:fhmonitor@127.0.0.1/fhmonitor",
+  "FH_MONGODB_CONN_URL":"mongodb://fhmonitor:fhmonitor@ds063879.mongolab.com:63879/fhhms",
   "FH_PORT":8801
 }
 var dynamic={
